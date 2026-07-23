a6b2e208ba6ea144f53ae9987cdfbd0cd0950e54
Juliana <juliana.rocha.dl@gmail.com>
2026-07-22 22:12:07 -0300

    Create exercicio2.sql

---
+++ exercicio2.sql
@@ -0,0 +1,6 @@
+-- Segundo exercicio realizado no RackerRank, onde foi solicitado o seguinte select: Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA' 
+  AND POPULATION > 120000;
