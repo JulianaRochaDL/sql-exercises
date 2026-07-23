a10ef141a4bc9530caa3bcfb67ee927cebb1e8f4
Juliana <juliana.rocha.dl@gmail.com>
2026-07-22 22:22:04 -0300

    Create exercicio6.sql

---
+++ exercicio6.sql
@@ -0,0 +1,6 @@
+-- Sexto exercicio realizado no RackerRank, onde foi solicitado o seguinte select: Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
