5bd4a7150d9a56e4d78f2a8ab45de84c6188c0af
Juliana <juliana.rocha.dl@gmail.com>
2026-07-22 22:16:35 -0300

    Create exercicio4.sql

---
+++ exercicio4.sql
@@ -0,0 +1,12 @@
+-- Quarto exercicio realizado no RackerRank, onde foi solicitado o seguinte select: Query all columns for a city in CITY with the ID 1661.
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
+
+
+
+
+
+
+
